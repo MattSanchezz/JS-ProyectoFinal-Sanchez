637030f1d015a3014b2f61755695e8dbf69ba65b
MattSanchezz <mati.sanchez.2001@hotmail.com>
2023-03-19 19:32:24 -0300

    Agregando ultimas modificaciones html css y js

--- cotiza/maincotiza.js
+++ cotiza/maincotiza.js
@@ -11,35 +11,35 @@ function convertir(){
     var Soles = 100;
     if (document.getElementById("uno").checked){
         resultado = valore / Dolar;
-        alert("El cambio de Pesos a Dolares es: $ " + resultado.toFixed(2));
+        alert("El cambio de Pesos a Dolares es: $ " + resultado.toFixed(2) + " Dolares");
     }
     else if (document.getElementById("dos").checked){
         resultado = valore / Euro;
-        alert("El cambio de Pesos a Euros es: $ " + resultado.toFixed(2));
+        alert("El cambio de Pesos a Euros es: $ " + resultado.toFixed(2) + " Euros");
     }
     else if (document.getElementById("cuatro").checked){
         resultado = valore / Uruguayo;
-        alert("El cambio de Pesos a Pesos Uruguayos es: $ " + resultado.toFixed(2));
+        alert("El cambio de Pesos a Pesos Uruguayos es: $ " + resultado.toFixed(2) + " Pesos Uruguayo");
     }
     else if (document.getElementById("cinco").checked){
         resultado = valore / Chileno;
-        alert("El cambio de Pesos a Pesos Chilenos es: $ " + resultado.toFixed(2));
+        alert("El cambio de Pesos a Pesos Chilenos es: $ " + resultado.toFixed(2) + " Pesos Chileno");
     }
     else if (document.getElementById("seis").checked){
         resultado = valore / Libras;
-        alert("El cambio de Pesos a Libras es: $ " + resultado.toFixed(2));
+        alert("El cambio de Pesos a Libras es: $ " + resultado.toFixed(2) + " Libras");
     }
     else if (document.getElementById("tres").checked){
         resultado = valore / Real;
-        alert("El cambio de Pesos a Reales es: $ " + resultado.toFixed(2));
+        alert("El cambio de Pesos a Reales es: $ " + resultado.toFixed(2) + " Reales");
     }
     else if (document.getElementById("siete").checked){
         resultado = valore / Guaranies;
-        alert("El cambio de Pesos a Guaranies es: $ " + resultado.toFixed(2));
+        alert("El cambio de Pesos a Guaranies es: $ " + resultado.toFixed(2) + " Guaranies");
     }
     else if (document.getElementById("ocho").checked){
         resultado = valore / Soles;
-        alert("El cambio de Pesos a Soles es: $ " + resultado.toFixed(2));
+        alert("El cambio de Pesos a Soles es: $ " + resultado.toFixed(2) + " Soles");
     }
     else{
         alert("Debe completar los campos para continuar")
